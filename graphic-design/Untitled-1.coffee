1fee1c7014da627764f2a62df8b5551212618646
AniaKB <aniakbennett@gmail.com>
2022-11-10 19:22:11 -0500

    website update/makeover

---
+++ graphic-design/Untitled-1.coffee
@@ -0,0 +1,21 @@
+
+hasTail = true
+kittenName = 'Fluffy'
+
+kitten = {
+    id: 35,
+    name: 'Fluffy'
+}
+
+kittens = [
+    { name: 'Fluffy' },
+    { name: 'Mittens' }
+]
+
+kittenNames = [
+    'Fluffy',
+    'Mittens'
+]
+
+kittenId = 35
+
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const skills = {
     Languages:  'JavaScript', 'HTML', 'CSS', 'Sass', 'SQL', 'Python',
-    Libraries_Frameworks:
+    Libraries_Frameworks: 'Vue.js', 'Bootstrap', 'jQuery', 'Lodash', 'date-fns',
+    Tooling: 'VSCode', 'Git', 'GitHub', 'GitHub Pages', 'ESLint', 'All Adobe CC Products',
+    Skills: 'Responsive Web Design', 'Wireframe mockups', 'Agile Certified Developer', 'CRM software',
 }
