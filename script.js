6421fc52d9c36519bcfdcc4a0b8d4420a00e58ab
AniaKB <aniakbennett@gmail.com>
2022-11-20 22:24:08 -0500

    fixing styling for paragraphs, margin

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const skills = {
-    Languages:  'JavaScript', 'HTML', 'CSS', 'Sass', 'SQL', 'Python',
-    Libraries_Frameworks: 'Vue.js', 'Bootstrap', 'jQuery', 'Lodash', 'date-fns',
-    Tooling: 'VSCode', 'Git', 'GitHub', 'GitHub Pages', 'ESLint', 'All Adobe CC Products',
-    Skills: 'Responsive Web Design', 'Wireframe mockups', 'Agile Certified Developer', 'CRM software',
+  Languages:  'JavaScript', 'HTML', 'CSS', 'Sass', 'SQL', 'Python',
+  Libraries_Frameworks: 'Vue.js', 'Bootstrap', 'jQuery', 'Lodash', 'date-fns',
+  Tooling: 'VSCode', 'Git', 'GitHub', 'GitHub Pages', 'ESLint', 'All Adobe CC Products',
+  Skills: 'Responsive Web Design', 'Wireframe mockups', 'Agile Certified Developer', 'CRM software',
 }
+
